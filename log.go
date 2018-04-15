c30d09eb4e1b4ccc517bbdb675bc7ade1a2f7b45
tr3e <807596654@qq.com>
2018-04-15 15:23:18 +0800

    add go file(s)

---
+++ log.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+const (
+	logDebug = iota
+	logInfo
+	logError
+	logFatal
+	logNull
+)
+
+var (
+	logLevel = logInfo
+)
+
+func debugLog(v ...interface{}) {
+	if logLevel <= logDebug {
+		prefixLog("[debug]", v...)
+	}
+}
+
+func debugLogf(format string, v ...interface{}) {
+	debugLog(fmt.Sprintf(format, v...))
+}
+
+func infoLog(v ...interface{}) {
+	if logLevel <= logInfo {
+		prefixLog("[info]", v...)
+	}
+}
+
+func infoLogf(format string, v ...interface{}) {
+	infoLog(fmt.Sprintf(format, v...))
+}
+
+func errorLog(v ...interface{}) {
+	if logLevel <= logError {
+		prefixLog("[error]", v...)
+	}
+}
+
+func errorLogf(format string, v ...interface{}) {
+	errorLog(fmt.Sprintf(format, v...))
+}
+
+func fatalLog(v ...interface{}) {
+	if logLevel <= logFatal {
+		log.Print("[FATAL]")
+		log.Panicln(v...)
+	}
+}
+
+func fatalLogf(format string, v ...interface{}) {
+	fatalLog(fmt.Sprintf(format, v...))
+}
+
+func prefixLog(pre string, v ...interface{}) {
+	if logLevel < logNull {
+		log.Println(pre, fmt.Sprint(v...))
+		// log.SetPrefix("")
+	}
+}
---
+++ main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"unicode"
+
+	link "github.com/tr3ee/go-link"
+)
+
+var (
+	lNetwork, lAddr string
+	rNetwork, rAddr string
+)
+
+func init() {
+	flag.StringVar(&lNetwork, "lNet", "tcp", "Specifies the protocol for listening.")
+	flag.StringVar(&rNetwork, "rNet", "tcp", "Specifies the remote protocol for dialing")
+	flag.StringVar(&lAddr, "lhost", "", "Specifies the address for listening.")
+	flag.StringVar(&rAddr, "rhost", "", "Specifies the remote address for dialing.")
+}
+
+func cLog(buf []byte) []byte {
+	var logStr string
+	for _, v := range buf {
+		if unicode.IsPrint(rune(v)) && v != '\n' {
+			logStr += string(v)
+		} else {
+			logStr += fmt.Sprintf("\\x%x", v)
+		}
+	}
+	infoLogf("Client:%s", logStr)
+	return buf
+}
+
+func sLog(buf []byte) []byte {
+	var logStr string
+	for _, v := range buf {
+		if unicode.IsPrint(rune(v)) && v != '\n' {
+			logStr += string(v)
+		} else {
+			logStr += fmt.Sprintf("\\x%02x", v)
+		}
+	}
+	infoLogf("Server:%s", logStr)
+	return buf
+}
+
+func main() {
+	flag.Parse()
+	if len(lAddr) == 0 || len(rAddr) == 0 {
+		flag.Usage()
+		return
+	}
+
+	l, err := net.Listen(lNetwork, lAddr)
+	if err != nil {
+		panic(err)
+	}
+	infoLogf("listening on %s:%s", lNetwork, lAddr)
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			log.Println(err)
+		}
+		go func(conn net.Conn) {
+			defer conn.Close()
+			connAddr, tgtAddr := conn.RemoteAddr(), conn.LocalAddr()
+
+			target, err := net.Dial(rNetwork, rAddr)
+			if err != nil {
+				errorLogf("failed to connect to %s:%s", rNetwork, rAddr)
+				return
+			}
+			defer target.Close()
+			infoLogf("Hijacking link %v <==> %v", connAddr, tgtAddr)
+			n1, n2, e1, e2 := link.TwoWayLink(nil, conn, target, cLog, sLog)
+			infoLogf("%v <==> %v (%d transmitted,%d received)", connAddr, tgtAddr, n1, n2)
+			if e1 != nil {
+				debugLogf(" %v --> %v: %s", connAddr, tgtAddr, e1)
+			}
+			if e2 != nil {
+				debugLogf(" %v <-- %v: %s", connAddr, tgtAddr, e1)
+			}
+		}(conn)
+	}
+}
